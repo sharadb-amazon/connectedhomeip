15e27a97fe1b457df73c257febe6c69b33c546d6
Sharad Binjola <sharadb@amazon.com>
2022-11-14 12:41:45 -0800

    ReadHandler: grab ExchangeManager from existing ExchangeContext Holder

--- src/app/ReadHandler.cpp
+++ src/app/ReadHandler.cpp
@@ -185,7 +185,12 @@ CHIP_ERROR ReadHandler::SendStatusReport(Protocols::InteractionModel::Status aSt
     {
         VerifyOrReturnLogError(!mExchangeCtx, CHIP_ERROR_INCORRECT_STATE);
         VerifyOrReturnLogError(mSessionHandle, CHIP_ERROR_INCORRECT_STATE);
-        auto exchange = InteractionModelEngine::GetInstance()->GetExchangeManager()->NewContext(mSessionHandle.Get().Value(), this);
+        // auto exchange = InteractionModelEngine::GetInstance()->GetExchangeManager()->NewContext(mSessionHandle.Get().Value(),
+        // this);
+
+        // grab ExchangeManager from existing ExchangeContext Holder
+        auto exchange = mExchangeCtx.Get()->GetExchangeMgr()->NewContext(mSessionHandle.Get().Value(), this);
+
         VerifyOrReturnLogError(exchange != nullptr, CHIP_ERROR_INCORRECT_STATE);
         mExchangeCtx.Grab(exchange);
     }
@@ -205,7 +210,12 @@ CHIP_ERROR ReadHandler::SendReportData(System::PacketBufferHandle && aPayload, b
     {
         VerifyOrReturnLogError(!mExchangeCtx, CHIP_ERROR_INCORRECT_STATE);
         VerifyOrReturnLogError(mSessionHandle, CHIP_ERROR_INCORRECT_STATE);
-        auto exchange = InteractionModelEngine::GetInstance()->GetExchangeManager()->NewContext(mSessionHandle.Get().Value(), this);
+        // auto exchange = InteractionModelEngine::GetInstance()->GetExchangeManager()->NewContext(mSessionHandle.Get().Value(),
+        // this);
+
+        // grab ExchangeManager from existing ExchangeContext Holder
+        auto exchange = mExchangeCtx.Get()->GetExchangeMgr()->NewContext(mSessionHandle.Get().Value(), this);
+
         VerifyOrReturnLogError(exchange != nullptr, CHIP_ERROR_INCORRECT_STATE);
         mExchangeCtx.Grab(exchange);
     }
