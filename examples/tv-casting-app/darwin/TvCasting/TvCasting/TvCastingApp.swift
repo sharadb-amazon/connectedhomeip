8a54f2a0b97c3a0bd821b19a0cf08d2baa1d116f
Sharad Binjola <sharadb@amazon.com>
2023-04-06 13:24:49 -0700

    Changing the way we listen to notifications

--- examples/tv-casting-app/darwin/TvCasting/TvCasting/TvCastingApp.swift
+++ examples/tv-casting-app/darwin/TvCasting/TvCasting/TvCastingApp.swift
@@ -18,8 +18,10 @@
 import SwiftUI
 import os.log
 
-@main
+
 struct TvCastingApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     let Log = Logger(subsystem: "com.matter.casting",
                      category: "TvCastingApp")
     @State
@@ -29,63 +31,119 @@ struct TvCastingApp: App {
         WindowGroup {
             ContentView()
                 .onAppear(perform: {
-                    if let castingServerBridge = CastingServerBridge.getSharedInstance()
-                    {
-                        let appParameters: AppParameters = AppParameters()
-                        
-                        var rotatingDeviceIdUniqueId: [UInt8] = [UInt8](repeating: 0, count: 16 )
-                        for i in (0...15)
-                        {
-                            rotatingDeviceIdUniqueId[i] = UInt8.random(in: 0..<255)
-                        }
-                        appParameters.rotatingDeviceIdUniqueId = Data(rotatingDeviceIdUniqueId)
-                        
-                        let onboardingParameters: OnboardingPayload = OnboardingPayload()
-                        onboardingParameters.setupPasscode = 20202021
-                        onboardingParameters.setupDiscriminator = 3840
-                        
-                        appParameters.onboardingPayload = onboardingParameters
-                        
-                        let err = castingServerBridge.initializeApp(appParameters, clientQueue: DispatchQueue.main, initAppStatusHandler: { (result: Bool) -> () in
-                            self.Log.info("initializeApp result \(result)")
-                        })
-                        self.Log.info("initializeApp return value \(err)")
-                    }
+
                 })
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-                    self.Log.info("TvCastingApp: UIApplication.willResignActiveNotification")
-                    if let castingServerBridge = CastingServerBridge.getSharedInstance()
-                    {
-                        castingServerBridge.stopMatterServer()
-                    }
+                /*.onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                    //self.Log.info("TvCastingApp: UIApplication.willResignActiveNotification")
+
                 }
                 .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
-                    self.Log.info("TvCastingApp: UIApplication.didBecomeActiveNotification")
-                    if(!firstAppActivation)
-                    {
-                        if let castingServerBridge = CastingServerBridge.getSharedInstance()
-                        {
-                            castingServerBridge.startMatterServer(DispatchQueue.main, startMatterServerCompletionCallback: { (error: MatterError) -> () in
-                                DispatchQueue.main.async {
-                                    self.Log.info("TvCastingApp.startMatterServerCompletionCallback called with \(error)")
-                                }
-                            })
-                        }
-                    }
-                    firstAppActivation = false
+                    //self.Log.info("TvCastingApp: UIApplication.didBecomeActiveNotification")
                 }
                 .onReceive(NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification)) { _ in
-                    self.Log.info("TvCastingApp: UIApplication.didEnterBackgroundNotification")
-                }
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didFinishLaunchingNotification)) { _ in
-                    self.Log.info("TvCastingApp: UIApplication.didFinishLaunchingNotification")
+                    //self.Log.info("TvCastingApp: UIApplication.didEnterBackgroundNotification")
                 }
                 .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-                    self.Log.info("TvCastingApp: UIApplication.willEnterForegroundNotification")
+                    //self.Log.info("TvCastingApp: UIApplication.willEnterForegroundNotification")
                 }
                 .onReceive(NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)) { _ in
-                    self.Log.info("TvCastingApp: UIApplication.willTerminateNotification")
-                }
+                    //self.Log.info("TvCastingApp: UIApplication.willTerminateNotification")
+                }*/
         }
+    }
+}
+
+@main
+class AppDelegate: NSObject, UIApplicationDelegate {
+    let Log = Logger(subsystem: "com.matter.casting",
+                     category: "AppDelegate")
+    @State
+    var firstAppActivation: Bool = true
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        self.Log.info("AppDelegate: UIApplication.didFinishLaunchingWithOptions")
+        
+        let window = UIWindow()
+        self.window = window
+        window.rootViewController = UIHostingController(rootView: ContentView())
+        window.makeKeyAndVisible()
+        
+        if let castingServerBridge = CastingServerBridge.getSharedInstance()
+        {
+            let appParameters: AppParameters = AppParameters()
+            
+            var rotatingDeviceIdUniqueId: [UInt8] = [UInt8](repeating: 0, count: 16 )
+            for i in (0...15)
+            {
+                rotatingDeviceIdUniqueId[i] = UInt8.random(in: 0..<255)
+            }
+            appParameters.rotatingDeviceIdUniqueId = Data(rotatingDeviceIdUniqueId)
+            
+            let onboardingParameters: OnboardingPayload = OnboardingPayload()
+            onboardingParameters.setupPasscode = 20202021
+            onboardingParameters.setupDiscriminator = 3840
+            
+            appParameters.onboardingPayload = onboardingParameters
+            
+            let err = castingServerBridge.initializeApp(appParameters, clientQueue: DispatchQueue.main, initAppStatusHandler: { (result: Bool) -> () in
+                self.Log.info("initializeApp result \(result)")
+            })
+            self.Log.info("initializeApp return value \(err)")
+        }
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        self.Log.info("AppDelegate: UIApplication.applicationWillResignActive")
+        if let castingServerBridge = CastingServerBridge.getSharedInstance()
+        {
+            castingServerBridge.stopMatterServer()
+        }
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        self.Log.info("AppDelegate: UIApplication.applicationDidBecomeActive")
+        if(!firstAppActivation)
+        {
+            if let castingServerBridge = CastingServerBridge.getSharedInstance()
+            {
+                castingServerBridge.startMatterServer(DispatchQueue.main, startMatterServerCompletionCallback: { (error: MatterError) -> () in
+                    DispatchQueue.main.async {
+                        self.Log.info("AppDelegate.startMatterServerCompletionCallback called with \(error)")
+                    }
+                })
+            }
+        }
+        firstAppActivation = false
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        self.Log.info("AppDelegate: UIApplication.applicationWillTerminate")
+/*        if let castingServerBridge = CastingServerBridge.getSharedInstance()
+        {
+            castingServerBridge.stopMatterServer()
+        }*/
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        self.Log.info("AppDelegate: UIApplication.applicationDidEnterBackground")
+        /*if let castingServerBridge = CastingServerBridge.getSharedInstance()
+        {
+            castingServerBridge.stopMatterServer()
+        }*/
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        self.Log.info("AppDelegate: UIApplication.applicationWillEnterForeground")
+        /*if let castingServerBridge = CastingServerBridge.getSharedInstance()
+        {
+            castingServerBridge.startMatterServer(DispatchQueue.main, startMatterServerCompletionCallback: { (error: MatterError) -> () in
+                DispatchQueue.main.async {
+                    self.Log.info("AppDelegate.startMatterServerCompletionCallback called with \(error)")
+                }
+            })
+        }*/
     }
 }
