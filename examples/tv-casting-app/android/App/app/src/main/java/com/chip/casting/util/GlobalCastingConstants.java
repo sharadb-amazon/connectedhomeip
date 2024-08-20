24040b2482a05b2f0649a4e480db2a17a3faee04
Sharad Binjola <sharadb@amazon.com>
2024-08-20 15:44:42 -0700

    Updating GlobalCastingConstants flag

--- examples/tv-casting-app/android/App/app/src/main/java/com/chip/casting/util/GlobalCastingConstants.java
+++ examples/tv-casting-app/android/App/app/src/main/java/com/chip/casting/util/GlobalCastingConstants.java
@@ -6,6 +6,6 @@ public class GlobalCastingConstants {
   public static final int SetupPasscode = 20202021;
   public static final int Discriminator = 0xF00;
   public static final boolean ChipCastingSimplified =
-      true; // set to true, to demo the simplified casting APIs. Otherwise, the older deprecated
+      false; // set to true, to demo the simplified casting APIs. Otherwise, the older deprecated
   // APIs are invoked
 }
